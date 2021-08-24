6e1b0e3d9469ec61bafd06e7c7f66646a363f841
SaurabhG02 <59133760+SaurabhG02@users.noreply.github.com>
2021-08-24 13:49:20 +0530

    by saurabh

--- djangoAPI/djangoAPI/settings.py
+++ djangoAPI/djangoAPI/settings.py
@@ -26,9 +26,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest-framework',
+    'corsheaders',
+    'Employee',
 ]
 
+CROS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
